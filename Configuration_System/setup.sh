b3ad9cbba05422af9eaf236ecffb98d2d4a5baa3
arturovictoriar <1231@holbertonschool.com>
2020-11-17 21:52:50 -0500

    Configuration instructions

---
+++ Configuration_System/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#Identify handwritten image using Tensoflow-Keras in CPU
+#Python version >= 3.5
+#Systems: Ubuntu 16.04.7 LTS - Ubuntu 20.04.1 LTS
+
+#update and upgrade Ubuntu
+sudo apt update -y
+sudo apt upgrade -y
+
+#install python3, pip, keras, cuda, matplotlib, cv2 and tensorflow cpu
+sudo apt install python3 python3.pip -y
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade setuptools
+sudo pip3 install tensorflow-cpu
+sudo pip3 install keras
+sudo apt install python3-matplotlib -y
+sudo apt install cuda-cudart-10-1 -y
+sudo pip3 install matplotlib
+sudo pip3 install opencv-python
+sudo apt install libgl1-mesa-glx -y
+
+# set the CPU
+export CUDA_VISIBLE_DEVICES=""
